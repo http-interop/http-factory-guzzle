cca669c1d56c0e65ec212349a58eb1bc346c58cf
Dirk Nederveen <dirk@zwartgeel.nl>
2021-07-06 09:56:46 +0200

    Allow guzzlehttp/psr7=^2.0, while retaining BC

--- src/StreamFactory.php
+++ src/StreamFactory.php
@@ -2,25 +2,44 @@
 
 namespace Http\Factory\Guzzle;
 
+use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Psr7\Utils;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\StreamInterface;
+
+use function GuzzleHttp\Psr7\stream_for;
+use function GuzzleHttp\Psr7\try_fopen;
 
 class StreamFactory implements StreamFactoryInterface
 {
     public function createStream(string $content = ''): StreamInterface
     {
-        return \GuzzleHttp\Psr7\stream_for($content);
+        if (\function_exists('stream_for')) {
+            // fallback for guzzlehttp/psr7<1.7.0
+            return stream_for($content);
+        }
+        return Utils::streamFor($content);
     }
 
     public function createStreamFromFile(string $file, string $mode = 'r'): StreamInterface
     {
-        $resource = \GuzzleHttp\Psr7\try_fopen($file, $mode);
+        if (\function_exists('try_fopen') && \function_exists('steam_for')) {
+            // fallback for guzzlehttp/psr7<1.7.0
+            $resource = try_fopen($file, $mode);
 
-        return \GuzzleHttp\Psr7\stream_for($resource);
+            return stream_for($resource);
+        }
+        $resource = Utils::tryFopen($file, $mode);
+
+        return new Stream($resource);
     }
 
     public function createStreamFromResource($resource): StreamInterface
     {
-        return \GuzzleHttp\Psr7\stream_for($resource);
+        if (\function_exists('stream_for')) {
+            // fallback for guzzlehttp/psr7<1.7.0
+            return stream_for($resource);
+        }
+        return new Stream($resource);
     }
 }
